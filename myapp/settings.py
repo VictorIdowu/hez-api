fe698ccb0aad366c80ce7ac6fe44c50b309d2857
VictorIdowu <idsonvictor@gmail.com>
2024-06-22 20:40:48 +0100

    kk

--- myapp/settings.py
+++ myapp/settings.py
@@ -128,8 +128,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_WHITELIST = (
+CORS_ALLOWED_ORIGINS = (
   'http://localhost:5173',
   'http://localhost:8000',
-  'http://localhost:3000'
+  'http://localhost:3000',
 )
